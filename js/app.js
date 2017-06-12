d754f566a05881850596988aa7eb42673651af3a
brandon <lrbn86@gmail.com>
2017-06-11 22:21:30 -0400

    Testing framework

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-alert("TEST");
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+
+function preload() {
+  game.load.image('test', 'assets/test.png');
+}
+
+function create() {
+  game.add.sprite(0, 0, 'test');
+}
+
+function update() {
+
+}
